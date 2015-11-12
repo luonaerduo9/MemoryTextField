d735af1892c91bd92189c4cafd41421673075446
ui01 <ui01@ui01s-mac-pro.local>
2015-11-12 14:08:18 +0800

    Initial Commit

---
+++ MemoryTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryTextField
+//
+//  Created by ui01 on 15/11/12.
+//  Copyright © 2015年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryTextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryTextField
+//
+//  Created by ui01 on 15/11/12.
+//  Copyright © 2015年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
